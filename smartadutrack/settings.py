5afe11684fe699c074c8ec326cffec3b0c1f9d06
Aditya13678 <adityaaa4689@gmail.com>
2025-09-16 16:49:29 +0530

    second

--- smartadutrack/settings.py
+++ smartadutrack/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'students',
+    'teachers',
+    'parents',
+    'attendance',
+    'assignments'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +132,5 @@ STATICFILES_DIRS=[BASE_DIR/'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='accounts.User'
